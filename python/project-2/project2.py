ee18e6b98e64b452e1cd7716936d51115ba0f09f
MAM <muzammilmohd538@gmail.com>
2024-06-23 20:37:30 +0530

    first commit

---
+++ python/project-2/project2.py
@@ -0,0 +1,6 @@
+name = input("Enter Name : ")
+age = input("Enter age : ")
+prof = input("Enter Profession : ")
+str = "hello {} , your age is {} and your profession is {}"
+print(str.format(name, age, prof))
+
